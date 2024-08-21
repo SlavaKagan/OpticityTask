27cd8903f77cfba22dc7dddedf0e8272d5501803
Slava <slava.kagan.ht@gmail.com>
2024-08-21 20:44:49 +0300

    Fix code

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 import app from './app';
+import { initializeDatabase } from './database/connection';
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+initializeDatabase().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
 });
